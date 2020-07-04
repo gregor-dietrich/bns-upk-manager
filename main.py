8f75f20e641b7d48d33cc2a333dbaf2fca13e505
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2020-07-04 11:53:56 +0200

    improved gamepath lookup in registry

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import json
 from os import mkdir, listdir, path, remove
 from shutil import copyfile
 from sys import exit
-from winreg import ConnectRegistry, EnumValue, HKEY_CURRENT_USER, OpenKey
+from winreg import ConnectRegistry, EnumValue, HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE, OpenKey
 
 charset = "utf-8"
 settings_location = "./settings.json"
-version = "0.21"
+version = "0.3"
 
 
 def init():
@@ -99,22 +99,50 @@ def checksum(file_name):
 
 
 def find_game_path():
+    # Try default install dir
     default = "C:/Program Files (x86)/NCSOFT/BnS/"
     if path.exists(default):
         return default
-    reg = ConnectRegistry(None, HKEY_CURRENT_USER)
-    key = OpenKey(reg, "Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\MuiCache\\")
+    # Search HKCU
+    result = search_reg("HKCU")
+    # Search HKLM
+    if result is None:
+        result = search_reg("HKLM")
+    # Fix Backslashes
+    if "\\" in result:
+        result = result.split("\\")
+        result = "/".join(result)
+    if path.exists(result):
+        return result
+
+
+def search_reg(scope):
+    if scope == "HKCU":
+        a_reg = ConnectRegistry(None, HKEY_CURRENT_USER)
+        a_key = "Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\MuiCache\\"
+    elif scope == "HKLM":
+        a_reg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+        a_key = "SOFTWARE\\WOW6432Node\\NCWest\\BnS\\"
+    else:
+        return
     count = 0
     try:
+        key = OpenKey(a_reg, a_key)
         while True:
             name, value, value_type = EnumValue(key, count)
-            if value == "Blade & Soul by bloodlust(x86)":
-                game_path = name.split(".")[0].split("\\")
-                game_path.pop()
-                game_path.pop()
-                return "/".join(game_path) + "/"
+            if scope == "HKCU":
+                if value == "Blade & Soul by bloodlust(x86)":
+                    game_path = name.split(".")[0].split("\\")
+                    game_path.pop()
+                    game_path.pop()
+                    log("Game path found in HKCU!")
+                    return "/".join(game_path) + "/"
+            elif scope == "HKLM":
+                if name == "BaseDir":
+                    log("Game path found in HKLM!")
+                    return value
             count += 1
-    except (WindowsError, OSError):
+    except (WindowsError, OSError, FileNotFoundError):
         pass
 
 
